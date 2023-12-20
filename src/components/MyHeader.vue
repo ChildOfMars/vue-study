d32f125ee63a24add57ad966ba14179c9d722748
Bing <2954316819@qq.com>
2023-12-20 09:43:23 +0800

    ''

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="todo-header">
+    <input v-model="text" @keyup.enter="addTodo" type="text" placeholder="请输入你的任务名称，按回车键确认" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyHeader",
+  props: ["addTodoObj"],
+  data() {
+    return {
+      text: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      if (!this.text.trim()) return;
+      this.addTodoObj(this.text);
+      this.text = "";
+    },
+  },
+};
+</script>
+
+<style scoped>
+/*header*/
+.todo-header input {
+  width: 560px;
+  height: 28px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 7px;
+}
+
+.todo-header input:focus {
+  outline: none;
+  border-color: rgba(82, 168, 236, 0.8);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
+}
+</style>
---
+++ src/components/MyList.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul class="todo-main">
+    <my-item v-for="todo in todos" :key="todo.id" :todo="todo" @updateTodoObj="updateTodoObj" @deleteTodoObj="deleteTodoObj"></my-item>
+  </ul>
+</template>
+  
+  <script>
+import MyItem from "./MyItem.vue";
+export default {
+  components: { MyItem },
+  name: "MyList",
+  props: ["todos"],
+  data() {
+    return {
+        
+    };
+  },
+  methods: {
+    updateTodoObj(id, done) {
+      this.$emit("updateTodoObj", id, done);
+    },
+    deleteTodoObj(id) {
+      this.$emit("deleteTodoObj", id);
+    },
+  },
+  
+};
+</script>
+  
+  <style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+</style>
